d9f10d3a1da451eb742596eca54b862ee2d29045
Andrea Begni <andrea.begni19@gmail.com>
2023-01-26 00:05:29 +0100

    Add libraries for graphics and add pie chart

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,13 +1,48 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Button } from "@react-native-material/core";
+import { VictoryLabel, VictoryPie } from "victory-native";
+import { Svg } from "react-native-svg";
 
 const Home = ({ navigation }) => {
+  const graphData = [
+    { y: 20, fill: "tomato" },
+    { y: 20, fill: "green" },
+    { y: 10, fill: "yellow" },
+  ];
+
+  const graphLabelText = "1000 Kcal";
+
   return (
     <View style={styles.container}>
       <Button
         title="pagina"
         onPress={() => navigation.navigate("Pagina", { name: "parametro" })}
       />
+
+      <Svg height={400} width={400}>
+        <VictoryPie
+          standalone={false}
+          width={400}
+          height={400}
+          data={graphData}
+          innerRadius={80}
+          style={{
+            labels: { display: "none" },
+            data: {
+              fill: ({ datum }) => datum.fill,
+            },
+          }}
+        />
+        <VictoryLabel
+          textAnchor="middle"
+          style={{ fontSize: 30, fontWeight: "bold" }}
+          x={200}
+          y={200}
+          text={graphLabelText}
+        />
+      </Svg>
+
       <StatusBar style="auto" />
     </View>
   );
